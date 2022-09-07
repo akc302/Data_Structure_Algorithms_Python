9b1a0e7ac633cc9ca0ba2755e27084514d005949
Ashraf Alam Khan Chowdhury <ashrafchowdhury302@gmail.com>
2022-09-06 22:56:03 -0400

    Data Structure & Algorithm Topics

--- Stack/StackList.py
+++ Stack/StackList.py
@@ -19,3 +19,19 @@ class Stack:
             return True
         else:
             return False
+
+    def push(self, value):
+        if self.isFull():
+            return "the stack is full"
+        else:
+            self.list.append(value)
+            return "the element is successfully added"
+
+
+cS = Stack(4)
+print(cS.isEmpty())
+print(cS.isFull())
+cS.push(11)
+cS.push(12)
+cS.push(13)
+print(cS)
