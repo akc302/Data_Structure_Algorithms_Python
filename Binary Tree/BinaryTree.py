87ca3c12a7e0087180e0d685c3a3a2e65e13d70e
Ashraf Alam Khan Chowdhury <ashrafchowdhury302@gmail.com>
2022-09-06 21:13:58 -0400

    Data Structure & Algorithm Topics

--- Binary Tree/BinaryTree.py
+++ Binary Tree/BinaryTree.py
@@ -4,7 +4,37 @@ class TreeNode:
         self.leftChild = None
         self.rightChild = None
 
-newBT = TreeNode("Drinks")   #o(1)
 
-#pre-order Traverse
-print("hey")
+newBT = TreeNode("Drinks")  # o(1)
+
+# pre-order Traverse
+leftChild = TreeNode("Hot")  # o(1)
+rightChild = TreeNode("Colds")  # o(1)
+
+newBT.leftChild = leftChild
+newBT.rightChild = rightChild
+
+
+def preOrderTraversal(rootNode):  # o(n)
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)  # O(n/2)
+    preOrderTraversal(rootNode.rightChild)  # O(n/2)
+
+
+# preOrderTraversal(newBT)
+
+# inOrder traversal in a binary Tree
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+# we can do the Post Order!
+# left node - right node - root
+
+# level Order Binary Tree
---
+++ Queue/QueueLinkedList.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        curNode = self.head
+        while curNode:
+            yield curNode
+            curNode = curNode.next
+
+
+class Queue:
+    def __init__(self):
+        self.linkedList = LinkedList()
+
+    def __str__(self):
+        values = [str(x) for x in self.linkedList]
+        return ' '.join(values)
+
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.linkedList.head == None:
+            self.linkedList.head = newNode
+            self.linkedList.tail = newNode
+        else:
+            self.linkedList.tail.next = newNode
+            self.linkedList.tail = newNode
+
+
+nQ = Queue()
+nQ.enqueue(1)
+nQ.enqueue(2)
+nQ.enqueue(3)
+
+print(nQ)
